edab1a83064e3ded953b501675602fff31521899
pandaman <fanda.alvaridzi01@gmail.com>
2025-01-27 22:09:47 +0700

    add:auth validate

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,6 +7,7 @@ const verifyToken = (req,res,next) =>{
     if(!token) return res.status(401).json({msg:"Unauthorization"})
 
         jwt.verify(token, process.env.JWT_SECRET,(err,decoded)=>{
+            //jika ada error kembalikan status 401 dengan pesan invalid token 
             if(err) return res.status(401).json({msg:"invalid token"});
             req.userId = decoded.id
             next();
---
+++ utils/validators/auth.js
@@ -0,0 +1,54 @@
+const {body} = require('express-validator')
+
+const prisma = require('../../prisma/client')
+
+const validateRegister =[
+    body('name').notEmpty().withMessage('Name is required'),
+    body('image').notEmpty().withMessage('Image is required')
+        .bail()
+        .custom((value,{req})=>{
+            if(!req.file)
+            {
+                throw new Error ('Image file is required')
+            }
+
+            // Periksa tipe file (hanya menerima JPEG, PNG, JPG)
+            const allowedMimeTypes = ['image/jpeg', 'image/png', 'image/jpg'];
+            if (!allowedMimeTypes.includes(req.file.mimetype)) {
+                throw new Error('Invalid image format. Only JPEG, PNG, and JPG are allowed');
+            }
+
+            // Periksa ukuran file (maks 5 MB)
+            const maxSize = 5 * 1024 * 1024; // 5 MB
+            if (req.file.size > maxSize) {
+                throw new Error('Image size must not exceed 5 MB');
+            }
+
+            return true;
+        })
+        ,
+    body('email')
+        .notEmpty().withMessage('Email is required')
+        .isEmail().withMessage('Email is required')
+        .custom(async (value)=>{
+            //Jika tidak memiliki value kembalikan/lemparkan Error
+            if(!value){
+                throw new Error('Email is required')
+            }
+
+            const user = await prisma.user.findUnique({where:{email:value}})
+            //Jika email sudah terdaftar kembalikan Error
+            if(user){
+                throw new Error('Email alerady exist')
+            }
+            return true;
+        }),
+    body('password').isLength({min:6}).withMessage('Password must be at least 6 characters long'),
+];
+
+const validateLogin = [
+    body('email').notEmpty().withMessage('Email is required'),
+    body('password').isLength({min:6}).withMessage('Password must be at'),
+]
+
+module.exports = { validateRegister,validateLogin}
